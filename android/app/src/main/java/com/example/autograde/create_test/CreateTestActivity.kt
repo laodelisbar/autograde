34cda015b6468c023cf853dd6b8e0ba16e82fba5
La Ode Lisbar <laodelisbar29@gmail.com>
2024-11-26 00:24:55 +0800

    FEAT : Halaman create test

---
+++ android/app/src/main/java/com/example/autograde/create_test/CreateTestActivity.kt
@@ -0,0 +1,21 @@
+package com.example.autograde.create_test
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.autograde.R
+
+class CreateTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_create_test)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
